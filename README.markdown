21bca8d74d172c685def34cf1103a996e6e4e55d
angeldollface <angeldollface666@gmail.com>
2023-01-05 15:55:45 +0100

    Init.

---
+++ README.markdown
@@ -0,0 +1,31 @@
+# CHROME QUOTLY :black_nib: :ribbon:
+
+***Get cute little quotes to brighten your day. :black_nib: :ribbon:***
+
+## ABOUT :bboks:
+
+Since I like to read quotes to cheer up or simply get inspired, I thought I'd write a small Chrome extension that shows you little quotes while you work on something. Enjoy. :heart_on_fire:
+
+## TRY THIS ON YOUR OWN MACHINE! :inbox_tray:
+
+- 1.) Download the release bundle from this repository's `Releases` section.
+- 2.) Unzip it.
+- 3.) Open Chrome and head to `chrome://extensions`.
+- 4.) Toggle `Developer Mode`.
+- 5.) Click `Load unpacked`.
+- 6.) Choose the folder you just unzipped!
+- 7.) Pin the extension.
+- 8.) Click the little `Q` icon.
+- 9.) Enjoy!
+
+## CHANGELOG :black_nib:
+
+### Version 1.0.0
+
+- Initial release.
+- Upload to GitHub.
+
+## NOTE :scroll:
+
+- *Chrome Quotly :black_nib: :ribbon:* by Alexander Abraham :black_heart: a.k.a. *"Angel Dollface" :dolls: :ribbon:*
+- Licensed under the MIT license.
---
+++ src/worker.js
@@ -0,0 +1,37 @@
+/*
+CHROME QUOTLY by Alexander Abraham, a.k.a. "Angel Dollface".
+Licensed under the MIT license.
+*/
+
+// Getting a random value from an object.
+function getRandomValue(object) {
+    let max = Object.keys(object).length - 1;
+    let min = 0;
+    let randKey = Object.keys(object)[getRandomIntInclusive(min, max)];
+    return object[randKey];
+}
+
+// Getting a random integer qith both "min"
+// and "max" included.
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+// Fetching a quote from my API and setting that to the main
+// content on the page. Refreshes every 30 seconds.
+function main(){
+    let apiURL = 'https://angeldollface.art/assets/apis/json/quotes.json';
+    let quote = '';
+    setInterval(
+        () => {
+            let element = document.getElementById('quote');
+            fetch(apiURL).then(res => res.json()).then(out => quote = getRandomValue(out)).catch(err => { console.log(err) });
+            element.innerHTML = quote;
+        },
+        6000
+    );
+}
+
+main();
